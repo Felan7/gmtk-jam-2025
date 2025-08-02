7ce0da0fb34ef789a8f9da6c7f33708f1686a843
Felan7 <felixkriegsmann@gmail.com>
2025-08-02 20:47:40 +0200

    Half way done enemy spawning

--- code/enemy.gd
+++ code/enemy.gd
@@ -6,9 +6,32 @@ var score_points = 1
 
 var score_text_animation_scene : PackedScene = preload("res://scenes/menu/score_text_animation.tscn")
 
+
+
 func _ready() -> void:
 	pass
 	# move out of view
+
+
+func create_random_position(player_position : Vector2, player_view_distance) -> Vector2 :
+	const MAX_X = 1080
+	const MAX_Y = 1080
+	const MIN_X = 0.0
+	const MIN_Y = 0.0
+
+	var random_position = Vector2(
+		randf_range(MIN_X, MAX_X),
+		randf_range(MIN_Y, MAX_Y)
+	)
+	if player_position.distance_to(random_position) < player_view_distance:
+		# move along
+		var lacking_distance = player_view_distance - player_position.distance_to(random_position)
+		# check if out of bounds
+		if random_position.x > MAX_X or random_position.x < MIN_X or random_position.y > MAX_Y or random_position.y < MIN_Y:
+			# try again
+			return create_random_position(player_position, player_view_distance)
+	return random_position
+
 
 func die() -> void:
 	print("Death is upon me says " + str(name))
