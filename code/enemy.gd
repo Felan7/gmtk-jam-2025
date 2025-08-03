812ea2ccf61200734457d889b8c267a5f9a78641
Felan7 <felixkriegsmann@gmail.com>
2025-08-03 18:26:27 +0200

    Frog Update

--- code/enemy.gd
+++ code/enemy.gd
@@ -92,7 +92,7 @@ func push_entities():
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") and body.has_method("impulse_effect"):
 		collidedNodes.append(body)
-		
+
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
