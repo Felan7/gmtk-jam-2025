6d6f6fbf207e25e525bb6bd56e37b7b659353266
veldoodles <35996699+veldoodles@users.noreply.github.com>
2025-08-03 15:07:02 +0300

    a2

--- code/wave_controller.gd
+++ code/wave_controller.gd
@@ -9,6 +9,7 @@ var enemies_in_wave : int = INF
 
 const wave_array = [
 	{"enemies" : [
+<<<<<<< Updated upstream
 		{"type" :"cat", "count" : 10},
 		{"type" :"brown_cat", "count" : 10},
 		{"type" :"chocolate_cat", "count" : 10},
@@ -20,6 +21,8 @@ const wave_array = [
 		{"type" :"snail", "count" : 10},
 		{"type" :"potted_plant", "count" : 1000},
 		{"type" :"snail", "count" : 9000},
+=======
+>>>>>>> Stashed changes
 		{"type" :"potted_plant", "count" : 1000},
 		{"type" :"snail", "count" : 9000},
 		{"type" :"squirrel", "count" : 10}],
@@ -82,11 +85,14 @@ func spawn_enemy(enemy_type, times) -> void:
 	for i in range(0, times):
 		print("Spawning enemy: " + enemy_type + " Nr. " + str(i))
 		var enemy = load("res://scenes/enemies/" + enemy_type +".tscn").instantiate()
+<<<<<<< Updated upstream
 		get_tree().root.add_child(enemy) # is this the proper way of doing things?
 		await get_tree().create_timer(0.1).timeout
 		get_tree().get_first_node_in_group("enemy_container").add_child(enemy) # Better way as we want to know where the enemies are and not spawn them inside the "main menu"
 		# get_tree().root.add_child(enemy) # is this the proper way of doing things?
 		await get_tree().process_frame # Wait one frame when spawning each time
+=======
+>>>>>>> Stashed changes
 		get_tree().get_first_node_in_group("enemy_container").add_child(enemy) # Better way as we want to know where the enemies are and not spawn them inside the "main menu"
 		# get_tree().root.add_child(enemy) # is this the proper way of doing things?
 		await get_tree().process_frame # Wait one frame when spawning each time
